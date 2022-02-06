f9803297eb75e7da3e36308e2f67c062607005cc
palhiman <palhimanshu786@gmail.com>
2022-02-06 18:31:39 +0530

    [update] contact form changed to get email notification

--- main.js
+++ main.js
@@ -21,12 +21,12 @@ var form = document.getElementById("contactForm");
             if (Object.hasOwn(data, 'errors')) {
               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
             } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
+              status.innerHTML = "Oops! There was a problem submitting your form";
             }
           })
         }
       }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
+        status.innerHTML = "Oops! There was a problem submitting your form";
       });
     }
-    form.addEventListener("submit", handleSubmit)
+    form.addEventListener("submit", handleSubmit);
